84089b2333ec1d7f5e471cc9761fe549018484e4
Nam Hyo Jin <sb04152@gmail.com>
2020-04-05 01:18:01 +0900

    Commit

---
+++ Graguation Portfolio/Shader/pixelshader.hlsl
@@ -0,0 +1,21 @@
+
+cbuffer alphaBuffer : register(b0)
+{
+	float alpha;
+}
+
+struct PS_INPUT
+{
+	float4 positon : SV_POSITION;
+	float2 texCoord : TEXCOORD;
+};
+
+
+Texture2D objTexture : TEXTURE: register(t0);
+SamplerState objSamplerState : SAMPLER: register(s0);
+
+float4 main(PS_INPUT input) : SV_TARGET
+{
+	float3 pixelColor = objTexture.Sample(objSamplerState, input.texCoord);
+	return float4(pixelColor, alpha);
+}
---
+++ Graguation Portfolio/Shader/vertexshader.hlsl
@@ -0,0 +1,25 @@
+
+cbuffer mycBuffer : register(b0)
+{
+	float4x4 mat;
+};
+
+struct VS_INPUT
+{
+	float3 position : POSITION;
+	float2 texCoord : TEXCOORD;
+};
+
+struct VS_OUTPUT
+{
+	float4 position : SV_POSITION;
+	float2 texCoord : TEXCOORD;
+};
+
+VS_OUTPUT main(VS_INPUT input)
+{
+	VS_OUTPUT output;
+	output.position = mul(float4(input.position, 1.0f), mat);
+	output.texCoord = input.texCoord;
+	return output;
+}
